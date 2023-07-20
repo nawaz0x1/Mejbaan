2e13391837fb9c04626c91d894a2270b49b4c157
Nawaz <nawazhaider60@gmail.com>
2023-07-20 21:07:33 +0600

    removed Parallel Routes

--- src/app/(protected)/dashboard/layout.jsx
+++ src/app/(protected)/dashboard/layout.jsx
@@ -1,19 +1,24 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Menubar from '@/components/Menubar';
 import { DataContextProvider } from '@/context/dataContext';
+import { useRouter } from 'next/navigation';
 
-export default function Dashboard({ find, map, provide }) {
+export default function Dashboard({ children }) {
+  const router = useRouter();
   const [menu, setMenu] = useState('find');
 
-  let item = menu === 'find' ? find : provide;
+  useEffect(() => {
+    const route = `/dashboard/${menu}`;
+    router.push(route);
+  }, [menu]);
 
   return (
     <>
       <Menubar menu={menu} setMenu={setMenu} />
       <main className="flex flex-col items-center">
-        <DataContextProvider>{item}</DataContextProvider>
+        <DataContextProvider>{children}</DataContextProvider>
       </main>
     </>
   );
